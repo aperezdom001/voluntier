81c74b7c3ee92f9d8d644b4c37cf769f01f1c39d
JL <jennalbouche@gmail.com>
2021-06-19 13:05:09 -0700

    add backend controlelrs, routes, models for user/posts, token auth, etc

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../controllers/users');
+const multer  = require('multer');
+const upload = multer();
+
+/*---------- Public Routes ----------*/
+router.post('/signup', upload.single('photo'), usersCtrl.signup);
+router.post('/login', usersCtrl.login);
+
+
+module.exports = router;
