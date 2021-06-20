656a106cf8b16c427bbbb62eeb388be4ea076d13
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-06-19 21:56:43 -0400

    Added Reviews.js

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -4,6 +4,23 @@ function Reviews() {
     return (
         <div>
             <h1>Reviews</h1>
+            <p> Ever since my surgery, I have become immobile. I posted on Voluntier
+                about my garden needing some tending to and within 3 hours I already
+                had members of the community responding to help me. I would reccommend 
+                Voluntier to everyone in need of assistance!
+                -Rosamund 71, Connecticut
+            </p>
+            <p>
+                We needed volunteers for a charity event for our local community center! Within hours we had 
+                responses and were able to fill our roster with people will to help! Thank you Voluntier!
+                -Darla 41, Massachussetts
+            </p>
+            <p>
+                Our non-profit WagMoreBarkLess needed help transporting puppies from a puppy mill down south. We 
+                organized our event on Voluntier and were able to find people on very short notice. Voluntier made
+                finding reputable volunteers extremely easy!
+                -Mary 35, Vermont
+            </p>
         </div>
     )
 }
