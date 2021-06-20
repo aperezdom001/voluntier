e2418f2f1183ee0c0d0177990deeb4fde9b18066
aperezdom <aideperz@gmail.com>
2021-06-19 18:03:56 -0600

    Added to opportuities file

---
+++ src/components/Opportunities.js
@@ -0,0 +1,24 @@
+export default function Opportunites(){
+    return(
+        <div>
+
+            <h1>Opportunities</h1>
+
+            <h2>Look for a cause you are interested in contributing to here!</h2>
+            <h2>Subjects include education, technology, social issues, and more!</h2>
+            
+            <ul>
+                <li>TA in public speaking class for high school students</li>
+                <li>Gardening weekend at Lynn Park to help the bees</li>
+                <li>5k Marathon donating to cancer patients at the Children's hospital</li>
+                <li>Build houses for people in need</li>
+                <li>Mentors needed for upcoming Hackathon for women and non-binary individuals</li>
+                <li>Volunteers needed for Poetry Slam</li> 
+                <li>Park cleanup at Swan's Lake</li>
+                <li>Donation drive for low income schools</li>
+
+            </ul>
+        </div>
+
+    )
+}
