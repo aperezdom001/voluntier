e07f26a32f6e70d8038d9331da6a8a8477141db1
aperezdom <aideperz@gmail.com>
2021-06-20 01:33:51 -0600

    Styled about home and reviews pages

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,26 +1,46 @@
 import React from 'react';
+import './Reviews.css';
 
 function Reviews() {
     return (
         <div>
-            <h1>Reviews</h1>
-            <p> Ever since my surgery, I have become immobile. I posted on Voluntier
+            <h1 className="rHeading">Reviews</h1>
+
+            <div className="review">
+            <p className="rDesc"> Ever since my surgery, I have become immobile. I posted on Voluntier
                 about my garden needing some tending to and within 3 hours I already
                 had members of the community responding to help me. I would reccommend 
                 Voluntier to everyone in need of assistance!
-                -Rosamund 71, Connecticut
             </p>
-            <p>
+
+            <p className="name">
+            -Rosamund 71, Connecticut
+            </p>
+
+            </div>
+
+            <div className="review">
+            <p className="rDesc">
                 We needed volunteers for a charity event for our local community center! Within hours we had 
                 responses and were able to fill our roster with people will to help! Thank you Voluntier!
-                -Darla 41, Massachussetts
             </p>
-            <p>
+
+            <p className="name">
+            -Darla 41, Massachussetts
+            </p>
+            </div>
+
+            <div className="review">
+            <p className="rDesc">
                 Our non-profit WagMoreBarkLess needed help transporting puppies from a puppy mill down south. We 
                 organized our event on Voluntier and were able to find people on very short notice. Voluntier made
                 finding reputable volunteers extremely easy!
-                -Mary 35, Vermont
             </p>
+            <p className="name">
+            -Mary 31, Connecticut
+            </p>
+            </div>
+
         </div>
     );
 }
